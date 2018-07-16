76ffaeaf4759491db38f8f84b3fb9408eec83a96
Gail Clement <gperetsm@caltech.edu>
2018-07-16 14:01:51 -0700

    Slightly revised based documents for all days

---
+++ data/insert_4b_DTtable.R
@@ -0,0 +1,17 @@
+# Annexes
+
+## Complete dataset `doaj_seal.csv` {#annex-table}
+
+```{r data-table}
+doaj_seal %>%
+  datatable(rownames = FALSE, 
+            colnames = c("Title", "Publisher", "Country", "Fees", "Waivers", "Identifiers", "Start Year", "Language(s)", "Review  Process", "Plagiarism check", "Time to Press", "License", "Author owns Copyright", "DOAJ Seal"),
+            class = "cell-border stripe", 
+            caption = "Journals with DOAJ Seal",
+            filter = list(position = "bottom"),
+            extensions = 'Buttons', options = list(dom = 'Bfrtip',
+                                                   buttons = c('colvis', 'csv', 'pdf'))
+  )
+
+```
+
